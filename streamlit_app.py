007dc6cfc0ca2fc5c0bf29d4519cd3afa5d8f7d3
Neeraja V <neerajavudatha@gmail.com>
2023-06-11 22:09:48 +0530

    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 
 import streamlit
 
-streamlit.title('My parents Healthy diner')
+streamlit.title('My parents New Healthy Diner')
 
-streamlit.text('Idly')
-streamlit.text('Dosa')
-streamlit.text('Upma')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
