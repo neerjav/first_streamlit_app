9fd5645aec9f7d1ef8b44b3549264ed72ffae425
Neeraja V <neerajavudatha@gmail.com>
2023-06-19 23:49:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,15 +66,16 @@ def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
         for fruit in new_fruit_list:
             my_cur.execute("insert into fruit_load_list values ('" + fruit + "')")
-            return 'Thank you for adding '+ new_fruit
+            streamlit.text('Thank you for adding '+ fruit)
         my_cnx.close()
         
         
 add_my_fruit = streamlit.text_input('What fruit would you like to add?') 
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    streamlit.text(back_from_function)
+    insert_row_snowflake(add_my_fruit)
+    #back_from_function = insert_row_snowflake(add_my_fruit)
+    #streamlit.text(back_from_function)
 
 
  
